9b89addf33f13f4c19f772923c9b833d2a0608a0
asmabouziri <asmabouziri299@gmail.com>
2024-07-04 10:49:19 +0100

    test

---
+++ src/compa/AppTest.java
@@ -0,0 +1,18 @@
+package compa;
+
+public class AppTest
+{
+   
+    public void testAppConstructor() {
+        App app1 = new App();
+        App app2 = new App();
+        app1.getMessage().equals(app2.getMessage())  ;
+    }
+
+    
+    public void testAppMessage()
+    {
+        App app = new App();
+       "Hello World!".equals(app.getMessage());
+    }
+}
