e530a3fe6ebde993102fea42f6bfee7458a39953
asmabouziri <asmabouziri299@gmail.com>
2024-07-05 15:13:57 +0100

    commit 10

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,17 +1,17 @@
-
-
 import compa.App;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest
 {
-   
+    @Test
     public void testAppConstructor() {
         App app1 = new App();
         App app2 = new App();
         app1.getMessage().equals(app2.getMessage())  ;
     }
 
-    
+    @Test
     public void testAppMessage()
     {
         App app = new App();
